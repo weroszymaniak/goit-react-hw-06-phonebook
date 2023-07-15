d25e05818c28ca4a2dc678c7e60c292cdea3f92a
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-07-15 08:55:53 +0200

    fix deleting issue

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
+
 const savedContacts = localStorage.getItem('contacts');
 const parsedContacts = JSON.parse(savedContacts) || [];
 
 const initialState = parsedContacts;
+
 const contactSlice = createSlice({
   name: 'contacts',
   initialState,
@@ -11,11 +13,13 @@ const contactSlice = createSlice({
       state.push(action.payload);
       localStorage.setItem('contacts', JSON.stringify(state));
     },
-  },
-  removeContact: (state, action) => {
-    const index = state.findIndex(contact => contact.id === action.payload);
-    state.splice(index, 1);
-    localStorage.setItem('contacts', JSON.stringify(state));
+    removeContact: (state, action) => {
+      const index = state.findIndex(contact => contact.id === action.payload);
+      if (index !== -1) {
+        state.splice(index, 1);
+        localStorage.setItem('contacts', JSON.stringify(state));
+      }
+    },
   },
 });
 
